ea4bcc79c379116c82771baa1c9a0f7735059aea
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-09-21 07:29:47 +0530

    connected netlify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lets do it
+          T Maa ka bharosa
         </a>
       </header>
     </div>
