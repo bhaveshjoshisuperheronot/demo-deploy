c51d0e53d3651ff7fdd1f0f824e9b9ee5eeefb03
bhaveshjoshisuperheronot <bhaveshjoshi011@gmail.com>
2020-09-26 08:04:24 +0530

    Video Js added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,39 @@
-import React  from 'react'
+import React, { Component } from 'react'
 import '../css/home.css'
-const Home = () => {
-    return(
+import VideoPlayer from 'react-video-js-player'
+import Demo from '../media/demo.mp4'
+const videoSrc = Demo;
+class Home extends Component {
+    render(){
+        return(
         <div className="wrapper">
-            <h1 className="center">Home</h1>
+            <div className="banner-video">
+            <VideoPlayer
+                controls={true}
+                src={videoSrc}
+                width="720"
+                height="420"
+                videoWidth="100%"
+                videoHeight="100%"
+            />
+            </div>
             <div className="row clearfix">
                 <div className="col-left left">
                     <div className="row-left row-show-list">English Channels</div>
-                    <div className="row-main row-left row-left-1">
+                    <div className="row-main row-left row-left-1 active">
                         <div className="bokoo-genre">Entertainment</div>
                         <div className="bokoo-number">36</div>
-                        <div className="bokoo-brand-img"><img alt='brand-img' src={require('../media/bokoo-brand-1.png')} /></div>
+                        <div className="bokoo-brand-img"><img alt='brand-img' src={require('../media/bokoo-brand-3.png')} /></div>
                     </div>
                     <div className="row-main row-left row-left-1">
                         <div className="bokoo-genre">Comedy</div>
                         <div className="bokoo-number">23</div>
-                        <div className="bokoo-brand-img"><img alt='brand-img' src={require('../media/bokoo-brand-2.png')} /></div>
+                        <div className="bokoo-brand-img"><img alt='brand-img' src={require('../media/bokoo-brand-1.png')} /></div>
                     </div>
                     <div className="row-main row-left row-left-1">
                         <div className="bokoo-genre">Action</div>
                         <div className="bokoo-number">73</div>
-                        <div className="bokoo-brand-img"><img alt='brand-img' src={require('../media/bokoo-brand-3.png')} /></div>
+                        <div className="bokoo-brand-img"><img alt='brand-img' src={require('../media/bokoo-brand-1.png')} /></div>
                     </div>
                     <div className="row-main row-left row-left-1">
                         <div className="bokoo-genre">Sci-fi</div>
@@ -46,7 +59,7 @@ const Home = () => {
                         <div className="row-right-col-1">12:30 AM</div>
                         <div className="row-right-col-1">13:00 AM</div>
                     </div>
-                    <div className="row-right row-right-1">
+                    <div className="row-right row-right-1 active">
                         <div className="row-main row-right-col-1">
                             <h4 className="show-head">Malgudi Days</h4>
                             <p className="show-text">Malgudi Days is a collection of short stories by R. K. Narayan published in 1943 by Indian Thought Publications.</p>
@@ -120,8 +133,8 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        </div>);
+    }
 }
 
-export default Home
+export default Home;
